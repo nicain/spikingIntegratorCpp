72008112a701732f87683577746ae9e404eda0d8
nicain <nicain@amath.washington.edu>
2012-02-20 04:50:10 +0000

    update

--- main.cpp
+++ main.cpp
@@ -85,7 +85,7 @@ int main( int argc,      // Number of strings in array argv
 	cout << Network.UUID_string << endl;
 	
     // Monitor time, if you want:
-//    MonitorBrain brainMonitor(Network);
+    MonitorBrain brainMonitor(Network);
 	
 	// Backgroud populations:
 	PoolBGHPoissonSIP BGESel1("BGESel1", Network, NSel, recordBGSpikes, BgFRE, 0, tOn, tOff);
