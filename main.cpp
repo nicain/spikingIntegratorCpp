06b45bb4db5eca402d73dcbdc67a449be1ed7073
nicain <nicain@amath.washington.edu>
2012-02-20 21:30:04 +0000

    bug found

--- main.cpp
+++ main.cpp
@@ -91,7 +91,7 @@ int main( int argc,      // Number of strings in array argv
 	PoolBGHPoissonSIP BGESel1("BGESel1", Network, NSel, recordBGSpikes, BgFRE, 0, tOn, tOff);
 	PoolBGHPoissonSIP BGESel2("BGESel2", Network, NSel, recordBGSpikes, BgFRE, 0, tOn, tOff);
 	PoolBGHPoissonSIP BGENSel("BGENSel", Network, NNSel, recordBGSpikes, BgFRE, 0, tOn, tOff);
-	PoolBGHPoissonSIP BGI("BGI", Network, NI, recordBGSpikes, BgFRI, tOn, 0, tOff);
+	PoolBGHPoissonSIP BGI("BGI", Network, NI, recordBGSpikes, BgFRI, 0, tOn, tOff);
 		
 	// Input populations:
 	PoolBGHPoissonSIP InputSel1("InputSel1", Network, NSel, recordInputSpikes, InputPoolFRSel1, inputCorrelation, tOn, tOff);
