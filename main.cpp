50af0367b589588860b0c0bedd461438522a07ee
nicain <nicain@amath.washington.edu>
2012-03-28 19:16:30 +0000

    added initials

--- main.cpp
+++ main.cpp
@@ -80,7 +80,7 @@ int main( int argc,      // Number of strings in array argv
 	
 	// Main network:
 	Brain Network;
-    //SWK
+    //SWK NHC
     // Output UUID, so that FR can be automatically computed
 	cout << Network.UUID_string << endl;
 	
