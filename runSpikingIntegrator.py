c75a38b7f9e2451fbc681f0209006bdb4e5ea971
nicain <nicain@amath.washington.edu>
2012-06-13 17:57:52 +0000

    working

--- runSpikingIntegrator.py
+++ runSpikingIntegrator.py
@@ -9,6 +9,7 @@
 
 # Import Packages:
 import sys
+import os
 import argparse
 import time
 from subprocess import Popen as Popen
@@ -120,6 +121,13 @@ if args.thresholdTest == 1:
 	CNP.thresholdTestSpikesBGTooUUID(currUUID, list(np.linspace(1,5000,5000)),tOn=args.tOn)
 	CNP.thresholdTestCurrentUUID(currUUID, list(np.linspace(.000001,.2,5000)),tOn=args.tOn)
 	CNP.thresholdTestCurrentBGTooUUID(currUUID, list(np.linspace(.000001,20,5000)),tOn=args.tOn)
+
+	BGESel1FileName = CNP.findFileName([currUUID, ".ntf", "BGESel1"])[0]
+	BGESel2FileName = CNP.findFileName([currUUID, ".ntf", "BGESel2"])[0]
+
+	os.remove(BGESel1FileName)
+	os.remove(BGESel2FileName)
+
 		   
 tEnd = time.mktime(time.localtime())
 print 'Total Computation Time: ', time.strftime("H:%H M:%M S:%S",time.gmtime(tEnd - tBegin))
