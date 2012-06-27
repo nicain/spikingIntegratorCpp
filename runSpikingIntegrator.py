4cf49c69ff1ab673089e8477a13723ec493828e7
nicain <nicain@amath.washington.edu>
2012-06-27 00:47:58 +0000

    update

--- runSpikingIntegrator.py
+++ runSpikingIntegrator.py
@@ -119,8 +119,8 @@ if args.thresholdTest == 1:
     CNP.thresholdTestUUID(currUUID, list(np.linspace(.1,40,5000)),tOn=args.tOn)
     CNP.thresholdTestUUIDDiff(currUUID, list(np.linspace(.1,40,5000)),tOn=args.tOn)
 
-    CNP.thresholdTestSpikesUUID(currUUID, list(np.linspace(1,5000,5000)),tOn=args.tOn)
-    CNP.thresholdTestSpikesBGTooUUID(currUUID, list(np.linspace(1,5000,5000)),tOn=args.tOn)
+    CNP.thresholdTestSpikesUUID(currUUID, list(np.linspace(.1,5000,5000)),tOn=args.tOn)
+    CNP.thresholdTestSpikesBGTooUUID(currUUID, list(np.linspace(.1,5000,5000)),tOn=args.tOn)
 
 #    CNP.thresholdTestCurrentUUID(currUUID, list(np.linspace(.000001,.2,5000)),tOn=args.tOn)
 #    CNP.thresholdTestCurrentBGTooUUID(currUUID, list(np.linspace(.000001,20,5000)),tOn=args.tOn)
@@ -131,10 +131,10 @@ if args.thresholdTest == 1:
     CNP.thresholdTestICUUIDDiff(currUUID, list(np.linspace(.1,40,5000)),tOn=args.tOn, FRDelta=.5)
 
         
-    BGESel1FileName = CNP.findFileName([currUUID, ".ntf", "BGESel1"])[0]
-    BGESel2FileName = CNP.findFileName([currUUID, ".ntf", "BGESel2"])[0]
-    os.remove(BGESel1FileName)
-    os.remove(BGESel2FileName)
+#    BGESel1FileName = CNP.findFileName([currUUID, ".ntf", "BGESel1"])[0]
+#    BGESel2FileName = CNP.findFileName([currUUID, ".ntf", "BGESel2"])[0]
+#    os.remove(BGESel1FileName)
+#    os.remove(BGESel2FileName)
            
 tEnd = time.mktime(time.localtime())
 print 'Total Computation Time: ', time.strftime("H:%H M:%M S:%S",time.gmtime(tEnd - tBegin))
